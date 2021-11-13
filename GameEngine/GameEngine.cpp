a55e550f138b9ad62b13f15cd798d3d9c7a42214
EddieEddieXD <88453434+EddieEddieXD@users.noreply.github.com>
2021-11-12 23:40:47 -0500

    Update GameEngine.cpp

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -7,8 +7,7 @@
 #include <regex>
 #include "GameEngine.h"
 
-GameEngine::GameEngine() : input (""), state(0){
-    
+GameEngine::GameEngine() : input (""), state(0), numberOfPlayer(0){
 }
 
 // friend int GameEngine::getState(){
@@ -18,11 +17,13 @@ GameEngine::GameEngine() : input (""), state(0){
 GameEngine::GameEngine(const GameEngine& GE){
     state = GE.state;
     input = GE.input;
+    numberOfPlayer = GE.numberOfPlayer
 }
 
 GameEngine& GameEngine::operator=(const GameEngine& GE) {
     state = GE.state;
     input = GE.input;
+    numberOfPlayer = GE.numberOfPlayer
     return *this;
 }
 
@@ -54,6 +55,7 @@ void GameEngine::MapLoaded(){
     // cout << "input is: " << input; //****for debugging
     cp.commandCollection.pop_back(); //removes command from collection
     //cin >> this->input; //replaced by CommandProcessor.getCommand()
+    cout<< "Please choose the map:" << endl;
     if (regex_match(input, regex("(loadmap )(.*).map"))){
         MapLoader ml(input); //GameEngine uses Map for loadmap in StartUpPhase()
         cout<< "Map loading... will you validate the map?" << endl;
@@ -74,6 +76,8 @@ void GameEngine::MapLoaded(){
 
 void GameEngine::MapValidated(){
     cout<< "Map has been validated" << endl;
+    cout<<this->mainMap.isAValidMap()<<endl;
+            cout<< "Map has been validated" << endl;
     state = 3;
 }
 
@@ -85,15 +89,38 @@ void GameEngine::PlayersAdded(){
     cp.commandCollection.pop_back(); //removes command from collection
     //cin >> this->input; //replaced by CommandProcessor.getCommand()
     if (input == "addplayer_*"){ //TODO: replace * with regex
-        cout << "player has been added!" << endl;
-        cout << "would you like to add other one?" << endl;
-        cout << "enter (addplayer_*) for add other one, enter (gamestart) to start game with current players.";
+        cout << "would you like to add a player?" << endl;
+        cout << "enter (addplayer_*) for add a player, enter (gamestart) to start game with current players.";
         cin >> this->input;
         if (input == "addplayer_*"){ //TODO: replace * with regex
             cout << "Add other player!" << endl;
-            state = 3;
+            cout<< "Please enter the name of the player"<<endl;
+            string playerName;
+            cin>>playerName;
+            Player *p= new Player(playerName);
+            Players.push_back(*p);  
+            numberOfPlayer++;
+            cout << "Now you have " << numberOfPlayer << " player(s)."<< endl;
+            if(numberOfPlayer < 2){
+                cout << "the mininum player must have 2, please add other one" << endl;
+                state = 3;
+            }
+            else if (numberOfPlayer >= 2 && numberOfPlayer < 6){
+                cout << "You have enough player right now, please enter(yes)for add other, or enter(start) to play" << endl;
+                cin >> this->input;
+                if(input == "yes"){
+                    state = 3;
+                }
+                else if(input = "gamestart"){
+                    state = 4;
+                }
+            }
+            else if (numberOfPlayer == 6){
+                cout << "You have maxinum player 6, loop will return and you have to just start" << endl;
+                state = 3;
+            }
         }
-        else if (input == "gamestart"){
+        else if (input == "gamestart" && numberOfPlayer >=2 && numberOfPlayer <=6){
             cout << "Game starting!" << endl;
             state = 4;
         }
@@ -102,13 +129,73 @@ void GameEngine::PlayersAdded(){
 }
 
 void GameEngine::AssignReinforcement(){
-    cout << "Would you assign reinforcement?" << endl;
-    cout << "Please enter (yes) for assign reinforcement" << endl;
-    cin >> this->input;
-    if (input == "yes"){
-        cout << "Assigned" << endl;
-        state = 5;
+    for(int i=0;i<Players.size();i++){
+        cout << "We are assign reinforcement right now" << endl;
+         // Determing the number of new solider the player has
+
+        //Minimum number
+        int numberOfSoliders=3+Players[i].territories.size()/3;
+        vector<int> PlayerterritoriesID;
+            
+            //Back up
+        for(int j=0;j<Players[i].territories.size();j++){
+            PlayerterritoriesID.push_back(Players[i].territories[j]->getID()); 
+            }
+
+        sort(PlayerterritoriesID.begin(),PlayerterritoriesID.end());
+        vector<int> ContinentTerritoriesID; //This vector will allow to compare with each vector of territory of each continent
+            
+        for(int j=0;j<mainMap.continents.size();j++){ //for loop that iterates trought the continents
+            for (int k = 0; k < mainMap.continents[j].c_countries.size(); k++){  //for loop that iterates trought the countrie of the continents
+                ContinentTerritoriesID.push_back(mainMap.continents[j].c_countries[k].getID()); //store all the ID in the vector
+                }
+                sort(ContinentTerritoriesID.begin(),ContinentTerritoriesID.end());
+                if(includes(PlayerterritoriesID.begin(),PlayerterritoriesID.end()
+                    ,ContinentTerritoriesID.begin(),ContinentTerritoriesID.end())){ //compare
+
+                        cout<<Players[i].name+" owns "+mainMap.continents[j].continentName<<endl;
+                        cout<<"He will receive 7 more soliders"<<endl;
+                        numberOfSoliders+=7;
+
+                }
+                ContinentTerritoriesID.clear();
+                
+            }
+
+            cout<<"You have "+to_string(numberOfSoliders)+" in reinforcement"<<endl;
+            cout<<"Here is all your territories: "<<endl;
+
+             for (int j = 0; j < Players[i].territories.size(); j++){
+                    cout<<Players[i].territories[j]->toString()<<endl;
+             }
+
+             //Assigning reinforcement
+
+             while(numberOfSoliders!=0){
+                 int InputID;
+                 int backup;
+                 
+                 cout<<"Please enter the territory ID that you want to reinforce"<<endl;
+                 cin>>InputID;
+                 cout<<"Please enter the number of soliders you want to add"<<endl;
+                 cin>> backup;
+
+                for(int j=0;j<Players[i].territories.size();j++){
+                    if(InputID==Players[i].territories[j]->getID()){
+                        Players[i].territories[j]->numberOfSolider=Players[i].territories[j]->getSoliders()+backup;
+                        numberOfSoliders-=backup;
+                    }
+                }
+                cout<<"Here is the number of soliders remaining: "+ to_string(numberOfSoliders)<<endl;
+             }
+            cout << "'\nReinforcement Assigned for "+Players[i].name<< endl;
+            cout<<"Here is all your territories: "<<endl;
+             for (int j = 0; j < Players[i].territories.size(); j++){
+                    cout<<Players[i].territories[j]->toString()<<endl;
+             }  
+        
     }
+    state = 5;
 }
 
 void GameEngine::IssueOrders(){
@@ -169,6 +256,84 @@ void GameEngine::Win(){
     }
 }
 
+void GameEngine::Play(){
+    cout<< "Would you like to start the game?" << endl;
+    cout<< "enter (gamestart) to start the game." << endl;
+    cp.getCommand(); //prompts user for commands, stores in collection
+    input = cp.commandCollection.at(0).command; //accesses first command given
+    cp.commandCollection.pop_back(); 
+    if (input == "gamestart"){
+        cout << "We are preparing everythng you need" << endl;
+
+         //Deteriming the order
+                    
+            random_shuffle(Players.begin(),Players.end());
+            cout<<" The order of play is :"<<endl;
+            for(int i=0;i<Players.size();i++){
+                cout<< Players[i].name<<endl;
+            }
+
+            //Distrubting territories to players
+                   
+            if (mainMap.world.size()%Players.size()==0){
+                cout<<"The number of territory is divisble by the number of players"<<endl;
+                int TerritoriesForEachPlayer=mainMap.world.size()/Players.size();
+                cout<<"Each player will have "+to_string(TerritoriesForEachPlayer)+" territories"<< endl;
+            }
+            else{
+                cout<<"The number of territory is not divisible by the number of players, some player will have more territories than others"<<endl;
+            }
+            int counter=0;
+            for(int i=0;i<mainMap.world.size();i++){
+                cout<<counter<<endl;
+                //Territory *ptr=new Territory(mainMap.world[i]);
+                
+                Players[counter].setTerritories(mainMap.world[i]);
+                counter+=1;
+                if(counter==Players.size())counter=0;
+                //delete ptr;
+                //ptr=NULL;
+            }
+        
+            //Give 50 initals armies
+
+            for(int i=0;i<Players.size();i++){
+                int limit=Players[i].territories.size();
+                int counter=0;
+                for(int j=0;j<50;j++){
+                    Players[i].territories[counter]->numberOfSolider=Players[i].territories[counter]->getSoliders()+1;
+                    counter+=1;
+                    if (counter==limit)counter=0;
+                }
+            }
+
+            cout<<"The soliders have been installed"<<endl;
+            cout<<mainMap.toString()<<endl;
+            cout<<"Territories of each player"<<endl;
+            for (int i = 0; i <Players.size(); i++)
+            {
+                cout<<"\n"+Players[i].name+" has " + to_string(Players[i].territories.size())+ " territories:"<<endl;
+                for (int j = 0; j < Players[i].territories.size(); j++)
+                {
+                    cout<<Players[i].territories[j]->toString()<<endl;
+                }
+                
+            }
+            
+
+            // Each player draw 2 cards from the deck
+
+            //gameDeck=Deck(); //Initialized at the start
+                    
+            for(int i=0;i<Players.size();i++){
+                cout<<"\nIt is the turn of "+Players[i].name+" to draw 2 cards:"<<endl;
+                Players[i].hand->addCardToHand(gameDeck.draw());
+                Players[i].hand->addCardToHand(gameDeck.draw());
+            }
+    }
+    state = 5;
+}
+
 //Assignment 2 Part 2:
 void GameEngine::StartUpPhase(){
     while(state == 0){
@@ -183,6 +348,7 @@ void GameEngine::StartUpPhase(){
             PlayersAdded();
         }
         while (state == 4){
+            Play();
             AssignReinforcement();
             while (state == 5){
                 IssueOrders ();
@@ -198,25 +364,3 @@ void GameEngine::StartUpPhase(){
     }
 
 }
-
-// //Assignment 2 Part 2:
-// void GameEngine::StartUpPhase(){ //this takes the place of Loop()
-//     cout<< "Please load a map by entering (loadmap filename.map)" << endl;
-//     cp.getCommand(); //prompts user for commands, stores in collection
-//     input = cp.commandCollection.at(0).command; //accesses first command given
-//     cp.commandCollection.pop_back(); //removes command from collection
-//     //cin >> this->input; //replaced by CommandProcessor.getCommand()
-//     if (regex_match(input, regex("(loadmap )(.*)"))){
-//         MapLoader ml(input); //GameEngine uses Map for loadmap in StartUpPhase()
-//     }
-//     else if(input == "validatemap"){
-
-//     }
-//     else if(regex_match(input, regex("(addplayer )(.*)"))){
-
-//     }
-//     else if(input == "start"){
-        
-//     }
-
-// }
